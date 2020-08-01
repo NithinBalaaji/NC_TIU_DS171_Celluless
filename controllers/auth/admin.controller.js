e8b08c2fee4c79f5cd30770ba2d6473db798b5da
Adwaith <adwaith3208@gmail.com>
2020-08-01 09:40:56 +0530

    Add workflow controller.

--- controllers/auth/admin.controller.js
+++ controllers/auth/admin.controller.js
@@ -6,12 +6,25 @@
 //Importing utils
 
 
-exports.registerAdmin = async (req, res) => {
-    try{
-
-    } catch(error){
-        console.log(error.toString());
-    }
+exports.registerAdmin = (req, res) => {
+        User.register(new User({
+			username:req.body.username,
+			name: req.body.name,
+			email: req.body.email,
+			mobile: req.body.mobile,
+			password: req.body.password,
+			idFaculty: req.body.isFaculty,
+			isAdmin: req.body.isAdmin
+		}),req.body.password,(err,newUser)=>{
+			if (err) {
+				console.log(err)
+				return res.json({success: false, err: err});
+			} else {
+				passport.authenticate("local")(req,res,()=>{
+					return res.json({success: true});
+				});
+			}
+		})
 }
 
 exports.loginAdmin = async (req, res) => {
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -2,21 +2,54 @@
 // Importing config/env variables
 
 // Importing models
-
+Workflow = require("../models/workflow");
+Group = require("../models/group");
 //Importing utils
 
 
 exports.createWorkflow = async (req, res) => {
     try{
+        let workflow = new Workflow();
+        workflow.name = req.body.name;
+        workflow.fields=req.body.fields;
+        approvers = req.body.approvers;
 
+        for(let i=0; i<approvers.length; i++){
+            let grp = await Group.findById(approvers[i].grp).exec();
+
+            if(!grp){
+                return res.json({success: false});
+            }
+            else{
+                let approver = {};
+                approver.level = approvers[i].level;
+                approver.grp = grp;
+                approvers.push(approver);
+            }
+        }
+
+        await workflow.save();
+        return res.json({success: true});
     } catch(error){
-        console.log(error.toString());
+        res.json({success: false});
     }
 }
 
 exports.viewWorkflow = async (req, res) => {
     try{
+        let workflow = await Workflow.findById(req.body.workflowId).populate({
+            path : 'approvers.grp',
+            populate : {
+              path : 'members'
+            }
+          }).exec();
 
+        if(!workflow){
+            return res.json({success: false});
+        }
+        else{
+            return res.json({success: true, workflow: workflow});
+        }
     } catch(error){
         console.log(error.toString());
     }
@@ -24,8 +57,30 @@ exports.viewWorkflow = async (req, res) => {
 
 exports.editWorkflow = async (req, res) => {
     try{
-
+        let workflow = await Workflow.findById(req.body.workflowId).exec();
+        if(!workflow){
+            return res.json({success: false});
+        }
+        else{
+            workflow.name = req.body.name;
+            workflow.fields=req.body.fields;
+            approvers = req.body.approvers;
+    
+            for(let i=0; i<approvers.length; i++){
+                let grp = await Group.findById(approvers[i].grp);
+    
+                if(!grp){
+                    return res.json({success: false});
+                }
+                else{
+                    approvers.push(approvers[i]);
+                }
+            }
+    
+            await workflow.save();
+            return res.json({success: true});
+        }
     } catch(error){
-        console.log(error.toString());
+        res.json({success: false});
     }
 }
