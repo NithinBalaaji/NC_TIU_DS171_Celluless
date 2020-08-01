06f482718b780bdd1d40fc9c8c2614b302c20a02
Bharath Kumar Ravichandran <bharathkumarravichandran@gmail.com>
2020-08-01 10:05:34 +0530

    Feat: Add request controller

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -1,11 +1,12 @@
-
 // Importing config/env variables
 
 // Importing models
 const User = require("../models/user");
+const Request = require('../models/request');
+
 //Importing utils
 
-getNextApprover = (request) => {
+getNextApprover = async (request) => {
     let lvl = 0;
     for(let i=0; i<request.approvedBy.length; i++){
         lvl=Math.max(request.approvedBy[i].level, lvl);
@@ -32,15 +33,56 @@ getNextApprover = (request) => {
 
 exports.createRequest = async (req, res) => {
     try{
+        if(!req.body.workflowId || !req.body.approvers){
+            return res.json({success: false})
+        }
+        let workflowId = req.body.workflowId;
+        let approvers = [];
 
+        req.body.approvers.forEach( async (approver) => {
+            let user = await User.findById(approver.approverId);
+            if(!user){
+                console.log('User doesnt exists!');
+                return;
+            }
+            approvers.push({
+                approverId: user,
+                level: approver.level
+            });
+        });
+
+        // TODO: Write web3 call functions
+        let blockchainId = 'lalalala';
+
+        let request = new Request({
+            blockchainId: blockchainId,
+            workflowId: workflowId,
+            approvers: approvers,
+            approvedBy: []
+        });
+        await request.save();
+
+        console.log('Request created');
     } catch(error){
         console.log(error.toString());
     }
 }
 
 exports.viewRequest = async (req, res) => {
-    try{
+    try {
+        if(!req.body.requestId){
+            return res.json({success: false})
+        }
 
+        let requestId = req.body.requestId;
+        let request = await Request.findById(requestId).populate('approvers.approverId').populate('approvedBy.approverId').populate('workflow_id').exec();
+
+        if(!request){
+            return res.json({success: false});
+        }
+
+        console.log('View request');
+        return res.json({success: true, request: request});
     } catch(error){
         console.log(error.toString());
     }
@@ -48,7 +90,22 @@ exports.viewRequest = async (req, res) => {
 
 exports.approveRequest = async (req, res) => {
     try{
+        if(!req.body.requestId){
+            return res.json({success: false})
+        }
+        
+        let requestId = req.body.requestId;
 
+        let request = await Request.findById(requestId).populate('approvers.approverId').populate('approvedBy.approverId').populate('workflow_id').exec();
+
+        if(!request){
+            return res.json({success: false});
+        }
+
+        // TODO: web3 approve
+        
+        console.log('Approve request');
+        return res.json({success: true });
     } catch(error){
         console.log(error.toString());
     }
@@ -56,7 +113,22 @@ exports.approveRequest = async (req, res) => {
 
 exports.rejectRequest = async (req, res) => {
     try{
+        if(!req.body.requestId){
+            return res.json({success: false})
+        }
+        
+        let requestId = req.body.requestId;
 
+        let request = await Request.findById(requestId).populate('approvers.approverId').populate('approvedBy.approverId').populate('workflow_id').exec();
+
+        if(!request){
+            return res.json({success: false});
+        }
+
+        // TODO: web3 reject request
+        
+        console.log('Reject request');
+        return res.json({success: true });
     } catch(error){
         console.log(error.toString());
     }
@@ -64,7 +136,22 @@ exports.rejectRequest = async (req, res) => {
 
 exports.viewRequestCertificate = async (req, res) => {
     try{
+        if(!req.body.requestId){
+            return res.json({success: false})
+        }
+        
+        let requestId = req.body.requestId;
 
+        let request = await Request.findById(requestId).populate('approvers.approverId').populate('approvedBy.approverId').populate('workflow_id').exec();
+
+        if(!request){
+            return res.json({success: false});
+        }
+
+        // TODO: web3 get certificate data, generate pdf and render
+        
+        console.log('View certificate request');
+        return res.json({success: true });
     } catch(error){
         console.log(error.toString());
     }
